8579845f82e059b6b579c461589dba8fa1da3f23
joemarlo <joe.marlo.09@gmail.com>
2019-10-16 12:04:55 -0400

    updated string matching process for the subway station names; manually adjusted a few station names

--- Subway-turnstiles/Subway_turnstiles.R
+++ Subway-turnstiles/Subway_turnstiles.R
@@ -5,11 +5,14 @@ library(lubridate)
 library(DescTools)
 library(stringdist)
 library(parallel)
+source("Plots/ggplot-theme.R")
+
+project.path <- getwd()
 
 cpu.cores <- detectCores()
 # there are some major data cleaning issues
 
-turnt <- read_csv("Subway-turnstiles/Data/turnstile_190928.txt",
+turnstile.df <- read_csv("Subway-turnstiles/Data/turnstile_190928.txt",
                   col_types = cols(
                     DATE = col_date(format = "%m/%d/%Y"),
                     ENTRIES = col_double(),
@@ -21,17 +24,18 @@ turnt <- read_csv("Subway-turnstiles/Data/turnstile_190928.txt",
 # data clean up-------------------------------------------------------------------------
 
 # change names to proper case
-names(turnt) <- sapply(names(turnt), function(name) paste0(toupper(substr(name, 1, 1)), tolower(substring(name, 2))))
-names(turnt)[1] <- "Booth"
-names(turnt)[3] <- "SCP"
+toproper <- function(name) paste0(toupper(substr(name, 1, 1)), tolower(substring(name, 2)))
+names(turnstile.df) <- sapply(names(turnstile.df), toproper)
+names(turnstile.df)[1] <- "Booth"
+names(turnstile.df)[3] <- "SCP"
 
 #only include "regular" and "recover audit" measurements. Others include maintenance checks
-turnt <- turnt %>% filter(Desc == "REGULAR" | Desc == "RECOVR AUD")
+turnstile.df <- turnstile.df %>% filter(Desc == "REGULAR" | Desc == "RECOVR AUD")
 
 # calculate the difference in Entries within each grouping of Booth and  SCP (SCP is a group of turnstiles within a station)
 # the goal here is the calculate the average difference in Entries for a given turnstile
 # this assumes turnstiles are in the same order each time the data is updated by the MTA
-turnt$diff <- ave(turnt$Entries, turnt$Booth, turnt$SCP, FUN = function(x) c(0, diff(x)))
+turnstile.df$diff <- ave(turnstile.df$Entries, turnstile.df$Booth, turnstile.df$SCP, FUN = function(x) c(0, diff(x)))
 
 # here's a simpler example
 attach(warpbreaks)
@@ -41,34 +45,34 @@ detach(warpbreaks)
 rm(warpbreaks)
 
 # summary stats
-summary(turnt$diff) %>% as.vector() %>% scales::comma()
+summary(turnstile.df$diff) %>% as.vector() %>% scales::comma()
 
 # table of counts for given extreme breaks
 breaks <- c(-Inf, 0, 1000, 10000, 100000, Inf)
-hist(turnt$diff, breaks = breaks, plot = FALSE)$counts
+hist(turnstile.df$diff, breaks = breaks, plot = FALSE)$counts
 
 # remove any negative counts
-turnt <- turnt[turnt$diff > 0,]
+turnstile.df <- turnstile.df[turnstile.df$diff > 0,]
 
 # remove anything over 100,000
 # note: average daily ridership for the who system is 5.5mm
-turnt <- turnt[turnt$diff <= 100000,]
+turnstile.df <- turnstile.df[turnstile.df$diff <= 100000,]
 
 # check again for outliers
-hist(turnt$diff, breaks = breaks, plot = FALSE)$counts
+hist(turnstile.df$diff, breaks = breaks, plot = FALSE)$counts
 rm(breaks)
 
 
 # EDA ---------------------------------------------------------------------
 
 # top stations by day
-turnt %>%
+turnstile.df %>%
   group_by(Station, Date) %>%
-  summarize(Daily = sum(diff)) %>%
-  arrange(desc(Daily))
+  summarize(Daily.ridership = sum(diff)) %>%
+  arrange(desc(Daily.ridership))
 
 # station ridership by day of the week
-turnt %>%
+turnstile.df %>%
   group_by(wday(Date), Station) %>%
   summarize(Daily = sum(diff)) %>%
   rename(WeekDay = `wday(Date)`) %>%
@@ -76,7 +80,7 @@ turnt %>%
                                  Station == "GRD CNTRL-42 ST" ~ "#ColGroup2",
                                  TRUE ~ "ColGroup3")) %>%
   ggplot(aes(x = WeekDay, y = Daily, group = Station, color = Color.group)) +
-  geom_line() +
+  geom_line(alpha = 0.4) +
   scale_colour_manual(values = c("forestgreen", "#2b7551", "gray80")) +
   theme(legend.position = "none") +
   scale_x_continuous(breaks = 1:7,
@@ -86,8 +90,8 @@ turnt %>%
                      name = "Daily station ridership") +
   labs(title = "Daily subway ridership by station",
        subtitle = "September 2019") +
-  annotate("text", x = 6.5, y = 180000, label = 'bold("Penn")', parse =TRUE, color = "forestgreen") +
-  annotate("text", x = 6.5, y = 160000, label = 'bold("GCT")', parse =TRUE, color = "#2b7551") +
+  annotate("text", x = 6.7, y = 155000, label = 'bold("Penn")', parse =TRUE, color = "#2b7551") +
+  annotate("text", x = 6.7, y = 140000, label = 'bold("GCT")', parse =TRUE, color = "forestgreen") +
   light.theme
 
 # ggsave(filename = "Plots/Subway_ridership.svg",
@@ -105,34 +109,37 @@ turnt %>%
 # but dataset station names don't match so we need to fuzzy match
 
 #import list of stations and tidy | need this for lat long
-stations.df <- GET("https://data.cityofnewyork.us/api/views/kk4q-3rt2/rows.csv?accessType=DOWNLOAD")
-stations.df <- content(stations.df)
+stations.latlong.df <- GET("https://data.cityofnewyork.us/api/views/kk4q-3rt2/rows.csv?accessType=DOWNLOAD")
+stations.latlong.df <- content(stations.latlong.df)
 
 # change names to proper case
-names(stations.df) <- sapply(names(stations.df), function(name) paste0(toupper(substr(name, 1, 1)), tolower(substring(name, 2))))
+names(stations.latlong.df) <- sapply(names(stations.latlong.df), toproper)
 
 #seperate out the lat/long
-stations.df <- stations.df %>%
+stations.latlong.df <- stations.latlong.df %>%
   mutate(The_geom = str_remove(The_geom, "POINT [(]") %>% str_remove(., "[)]")) %>%
-  separate(The_geom, into = c("Lat", "Long"), sep = " ") %>%
+  separate(The_geom, into = c("Long", "Lat"), sep = " ") %>%
   mutate(Lat = as.double(Lat),
          Long = as.double(Long)) %>%
   rename(Station = Name) %>%
   select(Station, Lat, Long, Line)
 
 #check the station names; most station names don't match so need to fuzzy match
-unique(turnt$Station)
-unique(stations.df$Station)
+unique(turnstile.df$Station)
+unique(stations.latlong.df$Station)
 
 # need to match on station name and line; check line first then match name?
 # match subway line letters by splitting apart and checking individually
-# need to clean up stations.df$Line first; remove "express" so E doesn't match it
-stations.df$Line <- lapply(stations.df$Line, function(line) str_remove_all(line, "Express"))
-stations.df$Line <- sapply(stations.df$Line, function(line) str_split(line, "-"))
+# need to clean up stations.latlong.df$Line first; remove "express" so E doesn't match it
+stations.latlong.df$Line <- lapply(stations.latlong.df$Line, function(line) str_remove_all(line, "Express"))
+stations.latlong.df$Line <- sapply(stations.latlong.df$Line, function(line) str_split(line, "-"))
 
-# similarly clean up the equavalent column in turnt
-turnt$Linename <- sapply(turnt$Linename, function(line) str_split(line, ""))
+# similarly clean up the equavalent column in turnstile.df
+turnstile.df$Linename <- sapply(turnstile.df$Linename, function(line) str_split(line, ""))
 
+# make station names lower case so they match better
+stations.latlong.df$Station <- tolower(stations.latlong.df$Station)
+turnstile.df$Station <- tolower(turnstile.df$Station)
 
 get_new_name <- function(old.name, subway.lines){
   
@@ -143,10 +150,10 @@ get_new_name <- function(old.name, subway.lines){
   #   of new station news
   
   # count of how many stations are matched in the new data
-  match.counts <- sapply(stations.df$Line, function(x) sum(subway.lines %in% x))
+  match.counts <- sapply(stations.latlong.df$Line, function(x) sum(subway.lines %in% x))
   
   # find the station names that match within two stations or at least one station
-  match.stations <- stations.df$Station[match.counts >= max(1, max(match.counts) - 2)]
+  match.stations <- stations.latlong.df$Station[match.counts >= max(1, max(match.counts) - 2)]
   
   # now fuzzy match the station name within this list
   new.name <- stringsim(old.name, match.stations, method = "osa") %>% which.max(.) %>% match.stations[.]
@@ -154,28 +161,33 @@ get_new_name <- function(old.name, subway.lines){
   return(new.name)
 }
 
-
 # get only unique pairs of station and lines
-station_line_pairs <- turnt[,c("Station", "Linename")][!duplicated(turnt[,c("Station", "Linename")]),]
+station.line.pairs <- turnstile.df[,c("Station", "Linename")][!duplicated(turnstile.df[,c("Station", "Linename")]),]
 
 # apply the get_new_name function over the unique pairs data frame
-station_line_pairs$New.name <- lapply(1:nrow(station_line_pairs), function(index) {
-  get_new_name(station_line_pairs$Station[[index]], station_line_pairs$Linename[[index]])
+station.line.pairs$New.name <- lapply(1:nrow(station.line.pairs), function(index) {
+  get_new_name(station.line.pairs$Station[[index]], station.line.pairs$Linename[[index]])
 })
 
 # seems to still be some issues. e.g. below stations
-# 42 ST-PORT AUTH
-# 34 ST-HERALD SQ
+# 34 st-herald sq
+# 14 st-union sq
+# botanic garden
+# and more
+
+#manually fix a few station
+station.line.pairs$New.name[station.line.pairs$Station == "34 st-herald sq"] <- "herald sq - 34th st"
+station.line.pairs$New.name[station.line.pairs$Station == "14 st-union sq"] <- "union sq - 14th st"
 
 # unlist the new name and line
-station_line_pairs$Linename <- sapply(station_line_pairs$Linename, function(x) unlist(x) %>% paste(., collapse = "-"))
-turnt$Linename <- sapply(turnt$Linename, function(x) unlist(x) %>% paste(., collapse = "-"))
-station_line_pairs$New.name <- sapply(station_line_pairs$New.name, unlist)
+station.line.pairs$Linename <- sapply(station.line.pairs$Linename, function(x) unlist(x) %>% paste(., collapse = "-"))
+turnstile.df$Linename <- sapply(turnstile.df$Linename, function(x) unlist(x) %>% paste(., collapse = "-"))
+station.line.pairs$New.name <- sapply(station.line.pairs$New.name, unlist) #is this neccessary still?
 
 # merge with the new data frame to get lat long
-turnt_clean <- left_join(x = turnt, y = station_line_pairs, by = c("Station", "Linename"))
-turnt_clean <- turnt_clean %>% rename(Old.station.name = Station, Station = New.name)
-turnt_clean <- left_join(x = turnt_clean, y = stations.df[, c("Station", "Lat", "Long")], by = "Station")
+turnstile.clean.df <- left_join(x = turnstile.df, y = station.line.pairs, by = c("Station", "Linename"))
+turnstile.clean.df <- turnstile.clean.df %>% rename(Old.station.name = Station, Station = New.name)
+turnstile.clean.df <- left_join(x = turnstile.clean.df, y = stations.latlong.df[, c("Station", "Lat", "Long")], by = "Station")
 
 # download nyc map shapes and clean them up
 nyc.geojson <- httr::GET('https://data.cityofnewyork.us/api/geospatial/tqmj-j8zm?method=export&format=GeoJSON')
@@ -184,16 +196,14 @@ summary(nyc.neighborhoods)
 nyc.df <- broom::tidy(nyc.neighborhoods)
 
 # map of the cumulative monthly ridership for each subway station
-turnt_clean %>%
-  group_by(Station) %>%
-  summarize(Monthly.ridership = sum(diff),
-            Lat = mean(Lat),
-            Long = mean(Long)) %>%
+turnstile.clean.df %>%
+  group_by(Station, Linename, Lat, Long) %>%
+  summarize(Monthly.ridership = sum(diff)) %>%
   ggplot() +
   geom_polygon(data = nyc.df,
                aes(x = long, y = lat, group = group),
                fill = "gray80") +
-  geom_point(aes(x = Lat, y = Long, color = Monthly.ridership, size = Monthly.ridership)) +
+  geom_point(aes(x = Long, y = Lat, color = Monthly.ridership, size = Monthly.ridership), alpha = 0.5) +
   coord_quickmap(xlim = c(-74.05, -73.9),
                  ylim = c(40.65, 40.82)) +
   scale_color_continuous(guide = "none") +
@@ -205,12 +215,12 @@ turnt_clean %>%
   light.theme +
   theme(axis.title = element_blank())
 
-ggsave(filename = "Plots/Subway_map.svg",
-       plot = last_plot(),
-       device = "svg",
-       path = project.path,
-       width = 8,
-       height = 6)
+# ggsave(filename = "Plots/Subway_map.svg",
+#        plot = last_plot(),
+#        device = "svg",
+#        path = project.path,
+#        width = 8,
+#        height = 6)
 
 # scratch code ------------------------------------------------------------
 
@@ -325,16 +335,16 @@ entries_exits %>%
   geom_line()
 
 
-new.names <- lapply(1:nrow(turnt), function(line) {
+new.names <- lapply(1:nrow(turnstile.df), function(line) {
   
-  subway.lines <- turnt$Linename[[line]]
-  old.name <- turnt$Station[[line]]
+  subway.lines <- turnstile.df$Linename[[line]]
+  old.name <- turnstile.df$Station[[line]]
   
   # count of how many stations are matched in the new data
-  match.counts <- sapply(stations.df$Line, function(x) sum(subway.lines %in% x))
+  match.counts <- sapply(stations.latlong.df$Line, function(x) sum(subway.lines %in% x))
   
   # find the station names that match within one stations
-  match.stations <- stations.df$Station[match.counts >= max(match.counts) - 2]
+  match.stations <- stations.latlong.df$Station[match.counts >= max(match.counts) - 2]
   
   # now fuzzy match the station name within this list
   new.name <- stringsim(old.name, match.stations, method = "osa") %>% which.max(.) %>% match.stations[.]
@@ -344,7 +354,7 @@ new.names <- lapply(1:nrow(turnt), function(line) {
 
 
 #match the original names to the new names using stringsim method
-orig_names <- unique(turnt$Station)
+orig_names <- unique(turnstile.df$Station)
 new_names <- sapply(orig_names, function(x){
   index <- stringsim(x, unique(stations_df$STATION), method = "osa") %>% which.max(.)
   unique(stations_df$STATION)[index]
