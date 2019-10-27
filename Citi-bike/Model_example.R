5863543c6954de43cbf032820bfa6c0f8797b9a2
joemarlo <joe.marlo.09@gmail.com>
2019-10-26 20:18:14 -0400

    added starttime as a predictor

--- Citi-bike/Model_example.R
+++ Citi-bike/Model_example.R
@@ -24,7 +24,7 @@ samp.rows <- sample(n.pop.rows, size = samp.size, replace = FALSE)
 # pull in a local dataframe containing those sample rows
 #   and the columns to be used for modeling
 samp.df <- tbl(conn, "citibike.2019") %>%
-  select(Tripduration, Birth.year, Gender, Usertype) %>%
+  select(Tripduration, Starttime, Birth.year, Gender, Usertype) %>%
   filter(row_number() %in% samp.rows) %>%
   collect()
 
@@ -42,30 +42,29 @@ rm(FUNS, summary.stats)
 
 # remove outliers
 samp.df <- samp.df %>%
-  filter(Tripduration < 60*60, #limit to trips under an hour
+  filter(Tripduration < (60 * 60), #limit to trips under an hour
          Birth.year > 1900) #don't want those 100+ year olds in the dataset
 
-
-# build three models (note: these are bad models, it's just an exmaple)
-model.lm <- lm(Tripduration ~ Birth.year * Gender * Usertype, data = samp.df)
-model.glm <- glm(Tripduration ~ Birth.year * Gender * Usertype, data = samp.df, family = poisson)
-model.rf <- ranger(Tripduration ~ Birth.year + Gender + Usertype, data = samp.df)
+# build three models (note: these are poor models, it's just an exmaple)
+model.lm <- lm(Tripduration ~ Starttime * Birth.year * Gender * Usertype, data = samp.df)
+model.glm <- glm(Tripduration ~ Starttime * Birth.year * Gender * Usertype, data = samp.df, family = poisson)
+model.rf <- ranger(Tripduration ~ Starttime + Birth.year + Gender + Usertype, data = samp.df)
 
 # summary(model.lm)
 # summary(model.glm)
 # # treeInfo(model.rf)
 
-
-# visual comaparison of the models on the same data they were trained on
+# visual comparison of the models on the same data they were trained on
 samp.df %>%
   mutate(LM = predict(model.lm),
          GLM = predict(model.lm),
          RandomForest = model.rf$predictions) %>%
-  gather(key = model.type, value = "Predicted.trip.duration", -c("Tripduration", "Birth.year", "Gender", "Usertype")) %>%
+  gather(key = model.type, value = "Predicted.trip.duration", -c("Tripduration", "Starttime", "Birth.year", "Gender", "Usertype")) %>%
   ggplot(aes(x = Tripduration, y = Predicted.trip.duration, color = Birth.year)) +
   geom_point() +
   geom_abline(slope = 1, intercept = 0) +
-  coord_fixed(ratio = 5) +
+  coord_cartesian(xlim = c(0, (60 * 60)),
+                  ylim = c(0, (60 * 60))) +
   facet_grid(~ model.type) +
   light.theme
   
@@ -77,26 +76,25 @@ samp.df %>%
 # tidypredict_fit(model.rf)
 # parse_model(model.rf)
 
-
 # test the models on database
 # determine which rows from main dataset to validate against
 samp.size <- 10000
 samp.rows <- sample(n.pop.rows, size = samp.size, replace = FALSE)
 
+# visual comparison of the models on new data
 tbl(conn, "citibike.2019") %>%
   filter(row_number() %in% samp.rows) %>%
   tidypredict_to_column(model.lm, vars = "LM") %>%
   tidypredict_to_column(model.glm, vars = "GLM") %>%
-  select(LM, GLM, Tripduration, Birth.year, Gender, Usertype) %>%
-  filter(Tripduration < 60*60, #limit to trips under an hour
+  select(LM, GLM, Tripduration, Starttime, Birth.year, Gender, Usertype) %>%
+  filter(Tripduration < (60 * 60), #limit to trips under an hour
          Birth.year > 1900) %>%
   collect() %>%
-  gather(key = model.type, value = "Predicted.trip.duration", -c("Tripduration", "Birth.year", "Gender", "Usertype")) %>%
+  gather(key = model.type, value = "Predicted.trip.duration", -c("Tripduration", "Starttime", "Birth.year", "Gender", "Usertype")) %>%
   ggplot(aes(x = Tripduration, y = Predicted.trip.duration, color = Birth.year)) +
   geom_point() +
-  coord_fixed(ratio = 5) +
-  # scale_x_log10() +
-  # scale_y_log10() +
+  coord_cartesian(xlim = c(0, (60 * 60)),
+                  ylim = c(0, (60 * 60))) +
   geom_abline(slope = 1, intercept = 0) +
   facet_grid( ~ model.type) +
   light.theme
